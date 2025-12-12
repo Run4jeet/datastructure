5afbedb33fcaa8f2705a6c749493cc2b4d775cd6
ranjeet <kartikey2030@gmail.com>
2025-12-12 11:24:38 +0530

    basic linkedlist functions

--- linkedlist/linked_list.py
+++ linkedlist/linked_list.py
@@ -26,13 +26,216 @@ class LinkedList:
 
     def append(self,value):
         new_node = Node(value)
-        self.tail.next = new_node
-        self.tail = new_node
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
         self.length += 1
+        return True
+
+    def pop(self):
+        if self.length ==0:
+            return None
+        temp = self.head
+        pre = self.head
+        while (temp.next):
+            pre = temp
+            temp = temp.next
+
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head =new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length +=1
+        return True
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length ==0:
+            self.tail = None
+        return temp.value
+
+    def get_v1(self,index):
+        if index < 1 and index >= self.length:
+            return None
+        temp = self.head
+        for i in range(index):
+            temp = temp.next
+        return temp.value
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def set_v1(self, index,value):
+        if index < 0 or index >= self.length:
+            return False
+        temp = self.head
+        for i in range(index):
+            temp = temp.next
+        temp.value = value
+        return True
+
+    def set_value(self,index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insert_v1(self,index,value):
+        # 10 ->20 ->30
+
+        new_node = Node(value)
+        if index ==0:
+            self.prepend(value)
+            self.length +=1
+            return True
+        if self.length ==0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            temp = self.get(index - 1)
+            after = temp.next
+            temp.next = new_node
+            new_node.next = after
+        self.length += 1
+        return True
+
+    def insert(self,index, value):
+        if index < 0 or index >=self.length:
+            return False
+        if index ==0:
+            return self.prepend(value)
+        if index== self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index -1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length +=1
+        return True
+
+    def remove_v1(self,index):
+        #10->20->30->40>
+        if index < 0 or index >= self.length:
+            return None
+        temp = self.head
+        pre = self.head
+        for i in range(index):
+            pre = temp
+            temp = temp.next
+
+        if self.length ==0:
+            self.head = None
+            self.tail = Node
+
+        if index == 0:
+            self.head = temp.next
+            temp.next = None
+            return temp
+        if index == self.length-1:
+            self.tail = pre
+            pre.next = None
+            temp.next = None
+            return temp
+
+        pre.next = temp.next
+        temp.next = None
+        self.length -=1
+        return temp
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        before = None
+        after = temp.next
+        for i in range( self.length):
+            #before = temp
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+    def remove(self,index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length:
+            return self.pop()
+        pre = self.get(index -1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next= None
+        self.length -= 1
+        return temp
+
+
+    def reverse_v1(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        before = None
+        after = temp.next
+        #while temp.next:
+        for i in range(self.length):
+            after = temp.next
+            temp.next =before
+            before = temp
+            temp = after
 
 
 
 
-mylist = LinkedList(1)
-mylist.append(2)
+
+
+
+
+
+
+
+###### test the method here #####
+mylist = LinkedList(10)
+mylist.append(20)
+mylist.append(30)
 mylist.printlist()
+mylist.reverse()
+#mylist.reverse_v1()
+print("after reverse:\n")
+mylist.printlist()
+
+#mylist.prepend(5)
+#mylist.printlist()
+#print("test remove at index 1")
+#mylist.remove(2)
+#mylist.printlist()
+#mylist.insert(1,22)
+#mylist.set_v1(4,11)
+#print("get 0", mylist.get(4))
+#mylist.printlist()
+#mylist.pop_first()
+#print("after pop first \n",mylist.pop_first())
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -2,6 +2,12 @@ import unittest
 from linkedlist.linked_list import LinkedList
 
 class TestLinkedList(unittest.TestCase):
+
+    def setUp(self):
+        self.ll = LinkedList(10)
+        self.ll.append(20)
+        self.ll.append(30)
+
     def test_append(self):
         ll = LinkedList(10)
         ll.append(5)
@@ -10,5 +16,78 @@ class TestLinkedList(unittest.TestCase):
         self.assertEqual(ll.tail.value, 5)
         self.assertEqual(ll.head.value, 10)
 
+    def test_pop(self):
+        ll = LinkedList(10)
+        ll.append(5)
+        ll.pop()
+        self.assertEqual(ll.length, 1)
+        self.assertEqual(ll.tail.value, 10)
+        #self.assertEqual(ll.length, 2)
+
+    def test_prepend(self):
+        ll = LinkedList(10)
+        ll.append(20)
+        ll.append(30)
+        ll.prepend(5)
+        self.assertEqual(ll.length,4)
+        self.assertEqual(ll.head.value,5)
+
+    def test_pop_first_with_one_element(self):
+        ll = LinkedList(10)
+        ll.pop_first()
+        self.assertEqual(ll.length, 0)
+        self.assertEqual(ll.head, None)
+        self.assertEqual(ll.tail, None)
+
+    def test_pop_first_with_multiple_elements(self):
+        ll = LinkedList(10)
+        ll.append(20)
+        ll.append(30)
+        ll.pop_first()
+        self.assertEqual(ll.length, 2)
+        self.assertEqual(ll.head.value, 20)
+       # self.assertEqual(ll.tail, None)
+
+    def test_get(self):
+        self.ll.append(20)
+        self.ll.append(30)
+        self.ll.append(40)
+
+        self.assertEqual(self.ll.length, 4)
+        self.assertEqual(self.ll.get(1).value,20)
+
+
+    def test_set_value(self):
+        self.ll.append(40)
+        self.ll.set_value(1, 22)
+        self.assertEqual(self.ll.length,4)
+        self.ll.printlist()
+        self.assertEqual(self.ll.get(1).value,22)
+
+    def test_insert(self):
+        #10->20->30
+        self.ll.insert(0,5)
+        self.assertEqual(self.ll.head.value,5)
+        self.assertEqual(self.ll.tail.value,30)
+        self.assertEqual(self.ll.insert(1,11),True)
+
+    def test_remove(self):
+        self.ll.remove(1)
+        #self.ll.printlist()
+        #print(self.ll)
+        self.assertEqual(self.ll.length,2)
+        # test remove first
+        self.ll.remove(0)
+        self.assertEqual(self.ll.head.value,30)
+
+    def test_reverse(self):
+        ll = LinkedList(10)
+        ll.append(20)
+        ll.append(30)
+        ll.reverse()
+        self.assertEqual(ll.head.value , 30)
+        self.assertEqual(ll.tail.value,10)
+
 if __name__ == '__main__':
+
     unittest.main()
